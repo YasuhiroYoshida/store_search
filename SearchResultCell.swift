9e7d58a9aec27a079ba1ca7ac1bf4482a6be28fa
YasuhiroYoshida <yasuhiro.yoppu@gmail.com>
2015-11-17 14:48:18 +0900

    Improve the search bar and icon styles

--- SearchResultCell.swift
+++ SearchResultCell.swift
@@ -15,14 +15,15 @@ class SearchResultCell: UITableViewCell {
   @IBOutlet weak var artworkImageView: UIImageView!
 
     override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+      super.awakeFromNib()
+      let selectedView = UIView(frame: CGRect.zero)
+      selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 0.5)
+      selectedBackgroundView = selectedView
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+      super.setSelected(selected, animated: animated)
+      // Configure the view for the selected state
+  }
 
 }
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -32,6 +32,8 @@ class SearchViewController: UIViewController {
     tableView.registerNib(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.nothingFoundCell)
 
     tableView.rowHeight = 80
+
+    searchBar.becomeFirstResponder()
   }
 
   override func didReceiveMemoryWarning() {
